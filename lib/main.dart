fb96499e669ec4b3ba8b4bd6ed89f69e58327172
ardi rizmaldi <ardi.rizmaldi@dkatalis.com>
2021-05-10 22:33:09 +0800

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/routes_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
