2ff202c7a09320f87eb2090ab78f6269b9c3b109
ardi rizmaldi <ardi.rizmaldi@dkatalis.com>
2021-05-10 23:16:00 +0800

    If applied this commit will Add loading screen progress for registration & login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/utils/form_validation.dart';
 import 'package:flash_chat/screens/widgets/logo_widget.dart';
 import 'package:flash_chat/screens/widgets/button_widgets.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -13,60 +14,76 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String _email;
   String _password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              LogoWidget(
-                sizeHeight: 200.0,
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (val) => _email = val,
-                decoration: kInputDecorationTextfield.copyWith(
-                  hintText: 'Enter your email',
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                LogoWidget(
+                  sizeHeight: 200.0,
                 ),
-                validator: (val) => FormValidation.validateEmail(val),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextFormField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (val) => _password = val,
-                decoration: kInputDecorationTextfield.copyWith(
-                  hintText: 'Enter your password.',
+                SizedBox(
+                  height: 48.0,
                 ),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              ButtonWidget(
-                color: Colors.lightBlueAccent,
-                onNavigate: () => FormValidation.validateLoginForm(
-                  form: _formKey.currentState,
-                  email: _email,
-                  passsword: _password,
-                  context: context,
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (val) => _email = val,
+                  decoration: kInputDecorationTextfield.copyWith(
+                    hintText: 'Enter your email',
+                  ),
+                  validator: (val) => FormValidation.validateEmail(val),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-                title: 'Log In',
-              ),
-            ],
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (val) => _password = val,
+                  decoration: kInputDecorationTextfield.copyWith(
+                    hintText: 'Enter your password.',
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                ButtonWidget(
+                  color: Colors.lightBlueAccent,
+                  onNavigate: () {
+                    setState(() {
+                      isLoading = true;
+                    });
+
+                    FormValidation.validateLoginForm(
+                      form: _formKey.currentState,
+                      email: _email,
+                      passsword: _password,
+                      context: context,
+                    );
+
+                    Future.delayed(const Duration(milliseconds: 500), () {
+                      setState(() {
+                        isLoading = false;
+                      });
+                    });
+                  },
+                  title: 'Log In',
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/utils/form_validation.dart';
 import 'package:flash_chat/screens/widgets/logo_widget.dart';
 import 'package:flash_chat/screens/widgets/button_widgets.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import '../constants.dart';
 
@@ -12,6 +13,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
   String _email;
   String _password;
 
@@ -19,57 +21,72 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              LogoWidget(
-                sizeHeight: 200.0,
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                validator: (val) => FormValidation.validateEmail(val),
-                decoration: kInputDecorationTextfield.copyWith(
-                  hintText: 'Enter your email',
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                LogoWidget(
+                  sizeHeight: 200.0,
                 ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                onChanged: (val) => _email = val,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextFormField(
-                validator: (val) => FormValidation.validatePassword(val),
-                obscureText: true,
-                textAlign: TextAlign.center,
-                decoration: kInputDecorationTextfield.copyWith(
-                  hintText: 'Enter your password',
+                SizedBox(
+                  height: 48.0,
                 ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                onChanged: (val) => _password = val,
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              ButtonWidget(
-                color: Colors.blueAccent,
-                onNavigate: () => FormValidation.validateRegistrationForm(
-                  form: _formKey.currentState,
-                  email: _email,
-                  passsword: _password,
-                  context: context,
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  validator: (val) => FormValidation.validateEmail(val),
+                  decoration: kInputDecorationTextfield.copyWith(
+                    hintText: 'Enter your email',
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  onChanged: (val) => _email = val,
                 ),
-                title: 'Register',
-              ),
-            ],
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  validator: (val) => FormValidation.validatePassword(val),
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  decoration: kInputDecorationTextfield.copyWith(
+                    hintText: 'Enter your password',
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  onChanged: (val) => _password = val,
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                ButtonWidget(
+                  color: Colors.blueAccent,
+                  onNavigate: () {
+                    setState(() {
+                      isLoading = true;
+                    });
+
+                    FormValidation.validateRegistrationForm(
+                      form: _formKey.currentState,
+                      email: _email,
+                      passsword: _password,
+                      context: context,
+                    );
+
+                    Future.delayed(const Duration(milliseconds: 500), () {
+                      setState(() {
+                        isLoading = false;
+                      });
+                    });
+                  },
+                  title: 'Register',
+                ),
+              ],
+            ),
           ),
         ),
       ),
